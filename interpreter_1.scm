27937dd3ba1caea2c6250e7d638cbfca409929e1
chad.johnson.1996 <chad.johnson.1996@gmail.com>
2018-02-19 19:49:14 -0500

    commit

--- interpreter_1.scm
+++ interpreter_1.scm
@@ -3,9 +3,6 @@
 ;Griffin Saiia, Chad Johnson
 ;PLC Project 1
 
-;variable storage
-(define stateList '((return true false)(() #t #f)))
-
 ;creates the state
 (define createState
   (lambda ()
@@ -19,83 +16,24 @@
 ;done allows me to check for that cleanly
 (define done (list (void)))
 
+(define getState
+  (lambda (state key)
+    (cond
+      ((number? key) key)
+      ((boolean? key) key)
+      ((null? (car state)) (error "Variable must be assigned to before reference"))
+      ((equal? (caar state) key) (caadr state))
+      (else (getState (list (cdar state) (cdadr state)) key)))))
 
+(define updateState
+  (lambda (state lis)
+    (cond
+      ((null? (car state)) (list (cons (car lis) (car state)) (cons (cadr lis) (cadr state))))
+      ((equal? (caar state) (car lis)) (list (cons (car lis) (car state)) (cons (cadr lis) (cadr state))))
+      (else (list (cons (caar state) (updateState (list (cdar state) (cdadr state)))))))))
+      
 ;state update and get
 
-;whenever adding or updating variables
-;takes varPair - a list whose first element is the variable name, and whose second is the value
-;returns current stateList
-;test cases: (a 10), (a '()), (a 17)
-(define updateState
-  (lambda (varPair)
-    ;checks if variable exists, if it does it changes the value for it - if not, it adds variable and value
-    ;lis is the variable half of the stateList
-    ;index keeps track of how deep it goes into the variable array in case of a value change on a predefined variable
-    (define (updateList var value lis index)
-      (cond
-        ;if lis is null, then variable has not been defined, thus we just cons variable and value to stateList
-        ((null? lis) (set! stateList (list (cons var (car stateList)) (cons value (cadr stateList)))))
-        ;if var is equal to an active variable, the index is passed to changeValue
-        ((eq? var (car lis)) (set! stateList (list (car stateList) (changeValue value (cadr stateList) index))))
-        ;recursive call that increments index
-        (else (updateList var value (cdr lis) (+ index 1)))))
-    ;handles whenever a variable's value needs changed
-    (define (changeValue value lis j)
-      (cond
-        ;checks for null just in case index goes out of bounds
-        ((null? lis) (cadr stateList))
-        ;checks index against 0
-        ((eq? 0 j) (cons value (cdr lis)))
-        ;recursive call that decrements index until at target value
-        (else (cons (car lis) (changeValue value (cdr lis) (- j 1))))))
-    ;logic for running helpers
-    (cond
-      ;if passed a null, returns stateList
-      ;((null? varPair) stateList)
-      ((null? varPair) '())
-      ;this was a way to get updateState to accomplish two things, change stateList
-      ;and return stateList - see 'done' definition for further info
-      ;((equal? done varPair) stateList)
-      ((equal? done varPair) '())
-      ;recursive call feeding updateList's output into updateState
-      (else (updateState (list (updateList (car varPair) (cadr varPair) (car stateList) 0)))))))
-
-
-;takes variable
-;returns value or '() if no value, returns done if the variable is undefined
-(define getState
-  (lambda (var)
-    ;takes variable we're searching for, a list, and start index
-    ;returns index of variable if it exists
-    (define (checkList var lis index)
-      (cond
-        ;if lis is null, then variable has not been defined, thus invalid index is returned
-        ((null? lis) -1)
-        ;if equal, returns index
-        ((eq? var (car lis)) index)
-        ;iterates down list, increments index
-        (else (checkList var (cdr lis) (+ index 1)))))
-    ;takes list and target index
-    ;returns value at given index
-    (define (fetchValue lis index)
-      (cond
-        ;if passed -1, variable was never defined
-        ((eq? index -1) done)
-        ;once index gets down to 0, we have our value
-        ((eq? index 0) (car lis))
-        ;recursives down list decrementing index
-        (else (fetchValue (cdr lis) (- index 1)))))
-    ;logic for running helpers
-    (cond
-      ;if passed a null, returns null
-      ((null? var) '())
-      ((number? var) var)
-      ((boolean? var) var)
-      ;calls fetchValue on the value half of stateList, and the index returned by checkList
-      (else (fetchValue (cadr stateList) (checkList var (car stateList) 0))))))
-
-;
-;
 ;expression evaluators
 (define addHandler
   (lambda (lis)
@@ -235,18 +173,18 @@
 
 
 (define sInterpreter
-  (lambda (parsed)
+  (lambda (state parsed)
     (cond
       ((not (null? (getState 'return))) (getState 'return))
       ((null? parsed) (getState 'return))
-      (else (sInterpreter (cdr (cons (oMutate (car parsed)) (cdr parsed) )))))))
+      (else (sInterpreter (oMutate state (car parsed)) (cdr parsed) )))))))
 
 (define oMutate
-  (lambda (lis)
-    ((getMutator (car lis)) (cdr lis))))
+  (lambda (state lis)
+    ((getMutator (car lis)) state (cdr lis))))
 
 (define oEval
-  (lambda (lis)
+  (lambda (state lis)
     (cond
       ((null? lis) lis)
       ((not (list? lis)) (getState lis))
@@ -262,7 +200,7 @@
 
 (define fileInterpreter
   (lambda (fileName)
-    (maskReturn (sInterpreter (parser fileName)))))
+    (maskReturn (sInterpreter (createState) (parser fileName)))))
 
 
 ;test cases
